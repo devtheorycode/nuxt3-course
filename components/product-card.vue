2d60efde84205d997ee816f26c8c870b5e72354d
Bryanoxx <contact@bryanparasote.fr>
2022-02-09 15:43:13 +0000

    feat: components 'product-card' and 'cards-container'

---
+++ components/product-card.vue
@@ -0,0 +1,41 @@
+<script setup>
+  let isHovering = ref(false)
+
+  const props = defineProps({
+    name: {
+      type: String,
+      required: true
+    },
+    price: {
+      type: Number,
+      required: true
+    },
+    imageSrc: {
+      type: String,
+      required: true
+    },
+  })
+</script>
+
+<template>
+  <div
+    class="card card-compact rounded-none cursor-pointer"
+    @mouseover="isHovering = true"
+    @mouseout="isHovering = false"
+    >
+    <figure>
+      <img :class="isHovering ? 'scale-110' : ''" class="transition-transform duration-500" :src="imageSrc">
+    </figure> 
+    <div class="card-body">
+      <p>
+        <span class="relative">
+          {{ name }}
+          <span :class="isHovering ? 'w-full' : 'w-0'" class="absolute bottom-0 left-0 h-[1px] bg-base-content duration-500"></span>
+        </span>
+      </p>       
+      <h2 class="card-title">
+        {{ price }}€
+      </h2> 
+    </div>
+  </div>
+</template>
--- pages/category/[categoryName].vue
+++ pages/category/[categoryName].vue
@@ -1,5 +1,28 @@
+<script setup>
+  const products = [
+    {
+      name: 'Super Hoodie',
+      price: 19,
+      imageSrc: 'https://picsum.photos/id/1005/400/250'
+    },
+    {
+      name: 'Super Tshirt',
+      price: 14,
+      imageSrc: 'https://picsum.photos/id/1005/400/250'
+    }
+  ]
+</script>
+
 <template>
   <div>
     <h1 class="text-5xl text-center p-5">Catégorie : {{ $route.params.categoryName }}</h1>
+    <cards-container class="mt-10">
+      <product-card
+        v-for="product in products"
+        :name="product.name"
+        :price="product.price"
+        :imageSrc="product.imageSrc"
+        />
+    </cards-container>
   </div>
 </template>
