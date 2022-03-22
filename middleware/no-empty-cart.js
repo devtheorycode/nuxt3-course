368ec295eb92e243c4b5a0b08ce8a3b8dc7b9c59
Bryanoxx <contact@bryanparasote.fr>
2022-03-22 10:54:41 +0000

    fix: middleware no-empty-cart renvoyant parfois une erreur 500

--- middleware/no-empty-cart.js
+++ middleware/no-empty-cart.js
@@ -1,9 +1,9 @@
 export default defineNuxtRouteMiddleware((to, from) => {
   const { selectedProducts } = useCart()
   if (selectedProducts.value.length === 0) {
-    const { $toast } = useNuxtApp()
-    $toast.error('Votre panier est vide, impossible de continuer')
     if (from.name) {
+      const { $toast } = useNuxtApp()
+      $toast.error('Votre panier est vide, impossible de continuer')
       return abortNavigation()
     } else {
       return navigateTo('/')
